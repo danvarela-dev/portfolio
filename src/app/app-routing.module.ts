221e7b75aa5d8d7106f9897785b08864710b17b1
Daniel Varela <daniel38912@gmail.com>
2023-07-23 00:22:04 -0600

    add home route, responsiveness and theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './modules/cms/components/layout/layout.component';
 import { CmsModule } from './modules/cms/cms.module';
+import { HomeComponent } from './modules/home/components/home/home.component';
 
-const routes: Routes = [{ path: '', component: LayoutComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), CmsModule],
